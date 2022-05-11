b7bc6c18e0bce546f36490369fc15003b6fd49fa
Mahmoud Hussein Tayem <77802068+mahmoudhtayem87@users.noreply.github.com>
2022-05-12 00:52:40 +0400

    Initial commit

---
+++ src/liferay-animation/animation-page-end-scripts/main.js
@@ -0,0 +1,4 @@
+console.group('Animation Page End Scripts');
+console.log('fragmentElement', fragmentElement);
+console.log('configuration', configuration);
+console.groupEnd();
---
+++ src/liferay-animation/animation-resources/main.js
@@ -0,0 +1,4 @@
+console.group('Animation Resources');
+console.log('fragmentElement', fragmentElement);
+console.log('configuration', configuration);
+console.groupEnd();
